e4f667ba97384fbb83be74ebd13a4cae6fbb73c9
Tamara Pavlović <tamarrapavlovic@gmail.com>
2024-02-06 12:18:07 +0100

    finish

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,9 +1,9 @@
 //navigacija
 var linkovi = [
-    {name:"Home",      href:"index.html"},
-    {name:"Gallery",  href:"gallery.html"},
-    {name:"Contact Us",href:"contact.html"},
-    {name:"Author",    href:"author.html"}
+    {name: "Home", href: "index.html"},
+    {name: "Gallery", href: "gallery.html"},
+    {name: "Contact Us", href: "contact.html"},
+    {name: "Author", href: "author.html"}
 ];
 
 var divNav = document.querySelector(".main-nav");
@@ -11,7 +11,7 @@ var divNavMobile = document.querySelector(".mobile-nav");
 var navContent = `<ul class="main-nav__items">`;
 var navContentMobile = `<ul class="mobile-nav__items">`;
 
-linkovi.forEach(link =>{
+linkovi.forEach(link => {
     navContent += `<li class="main-nav__item">
                     <a href="${link.href}">${link.name}</a>
                     </li>`;
@@ -26,20 +26,18 @@ divNav.innerHTML = navContent;
 divNavMobile.innerHTML = navContentMobile;
 
 
-
 var backdrop = document.querySelector('.backdrop');
-var selectPlanButton = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 console.log(backdrop);
 
-backdrop.addEventListener('click',function(){
+backdrop.addEventListener('click', function () {
     //mobileNav.style.display = "none";
     mobileNav.classList.remove('open');
     backdrop.classList.remove('open');
 });
 
-toggleButton.addEventListener('click', function() {
+toggleButton.addEventListener('click', function () {
     //mobileNav.style.display = 'block';
     //backdrop.style.display = 'block';
     mobileNav.classList.add('open');
